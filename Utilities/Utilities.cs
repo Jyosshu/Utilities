c6267fc42a698b2a385dd37e591f5637693b97c0
Josh Wygle <jwygle@gmail.com>
2019-12-01 14:31:03 -0500

    first commit

---
+++ Utilities/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Utilities
+{
+    public class Utilities
+    {
+        static DirectoryInfo _outputDirectory = null;
+        public static DirectoryInfo OutputDirectory
+        {
+            get => _outputDirectory;
+            set
+            {
+                _outputDirectory = value;
+                if (!_outputDirectory.Exists)
+                {
+                    _outputDirectory.Create();
+                }
+            }
+        }
+    }
+}
